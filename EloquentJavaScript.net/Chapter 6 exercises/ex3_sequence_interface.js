90b0f58d3bf59645007d8bde2fa0cd032fddb654
Paul Koepke <paul.koepke@gmail.com>
2015-10-21 14:45:58 -0400

    Done with Eloquent Chapter 6 exercises

--- EloquentJavaScript.net/Chapter 6 exercises/ex3_sequence_interface.js
+++ EloquentJavaScript.net/Chapter 6 exercises/ex3_sequence_interface.js
@@ -10,7 +10,7 @@ Object.defineProperty(ArraySeq.prototype, "logFive", {
   }
 })*/
 
-/* This returns an entire array and lets logFive() handle the rest. Turns out the author wanted something more complex - return one element at a time, and have a way to determine if the sequence has ended.
+/* This returns an entire array and lets logFive() handle the rest. It passes the tests, but based on the "hints" in the text it turns out the author wanted something more complex - return one element at a time, and have a way to determine if the sequence has ended.
 Object.defineProperty(ArraySeq.prototype, "wrappedArrayGetter", {
   enumerable: false, // enumerable: false is the default setting, but I'm including it for clarity.
   get: function() {
@@ -18,34 +18,51 @@ Object.defineProperty(ArraySeq.prototype, "wrappedArrayGetter", {
   },
 })*/
 
+Object.defineProperty(ArraySeq.prototype, "getAnElementAndRestOfArray", {
+  enumerable: false, // enumerable: false is the default setting, but I'm including it for clarity.
+  value: function(whichElement) {
+    var arrayToReturn = [this.wrappedArray[whichElement], this.wrappedArray.slice(whichElement + 1)];
+    return arrayToReturn;
+  }
+})
+
 function RangeSeq(from, to) {
-  this.arrayOfIntegers = [];
+  this.wrappedArray = [];
   for(var i = from; i <= to; i++) {
-    this.arrayOfIntegers.push(i);
+    this.wrappedArray.push(i);
   }
 }
-
-/*
-Object.defineProperty(RangeSeq.prototype, "wrappedArrayGetter", {
-  enumerable: false, // enumerable: false is the default setting, but I'm including it for clarity.
-  get: function() {
-    return this.arrayOfIntegers;
-  },
-})*/
 
 /* Not the way the author wanted me to implement the interface.
 Object.defineProperty(RangeSeq.prototype, "logFive", {
   value: function() {
-    for(var i = 0; i < 5 && i < this.arrayOfIntegers.length; i++) {
-      console.log(this.arrayOfIntegers[i]);
+    for(var i = 0; i < 5 && i < this.wrappedArray.length; i++) {
+      console.log(this.wrappedArray[i]);
     }
   }
 })*/
 
+/* This returns an entire array and lets logFive() handle the rest. It passes the tests, but based on the "hints" in the text it turns out the author wanted something more complex - return one element at a time, and have a way to determine if the sequence has ended.
+Object.defineProperty(RangeSeq.prototype, "wrappedArrayGetter", {
+  enumerable: false, // enumerable: false is the default setting, but I'm including it for clarity.
+  get: function() {
+    return this.wrappedArray;
+  },
+})*/
+
+Object.defineProperty(RangeSeq.prototype, "getAnElementAndRestOfArray", {
+  enumerable: false, // enumerable: false is the default setting, but I'm including it for clarity.
+  value: function(whichElement) {
+    var arrayToReturn = [this.wrappedArray[whichElement], this.wrappedArray.slice(whichElement + 1)];
+    return arrayToReturn;
+  }
+})
+
 function logFive(arrayWrapper) {
-  var arrayToIterate = arrayWrapper.wrappedArrayGetter;
-  for(var i = 0; i < 5 && i < arrayToIterate.length; i++) {
-    console.log(arrayToIterate[i]);
+  for(var i = 0; i < 5; i++) {
+    returnedArray = arrayWrapper.getAnElementAndRestOfArray(i);
+    console.log(returnedArray[0]);
+    if(returnedArray[1].length == 0) break;
   }
 }
 
@@ -53,18 +70,21 @@ function logFive(arrayWrapper) {
 //console.log(new ArraySeq([1, 2]).wrappedArray);
 var arraySeqOne = new ArraySeq([1,2]);
 var arraySeqTwo = new ArraySeq([1,2,3,4,5,6,7]);
-console.log(arraySeqTwo.wrappedArray);
+//console.log(arraySeqTwo.getAnElementAndRestOfArray(1));
+logFive(arraySeqOne);
+logFive(arraySeqTwo);
 //console.log(arraySeqOne);
 //console.log(arraySeqOne.wrappedArray);
 
 //arraySeqOne.logFive();
 //arraySeqTwo.logFive();
-logFive(arraySeqTwo);
 
-var rangeSeqOne = new RangeSeq(1,7);
+var rangeSeqOne = new RangeSeq(1,2);
+var rangeSeqTwo = new RangeSeq(1,8);
 logFive(rangeSeqOne);
+logFive(rangeSeqTwo);
 //console.log(rangeSeqOne);
 //rangeSeqOne.logFive();
 
-var giantRangeSeq = new RangeSeq(0,10000000);
-console.log(giantRangeSeq.wrappedArrayGetter.length);
+//var giantRangeSeq = new RangeSeq(0,10000000);
+//console.log(giantRangeSeq.wrappedArrayGetter.length);
